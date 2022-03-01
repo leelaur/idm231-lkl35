fc41098c45615005dc64a04e53c3fe88f2189e10
leelaur <laurenlee552@yahoo.com>
2022-03-01 17:40:28 -0500

    :smirk:
    make html button
    start js const
    add transition

---
+++ main.js
@@ -0,0 +1,13 @@
+const jellyBtn = document.getElementById ('jelly');
+const bostonBtn = document.getElementById ('boston');
+const oldfBtn = document.getElementById ('oldf');
+const vanillaBtn = document.getElementById ('vanilla');
+const strawberryBtn = document.getElementById ('strawberry');
+const blueberryBtn = document.getElementById ('blueberry');
+const barvarianBtn = document.getElementById ('barvarian');
+const cruellerBtn = document.getElementById ('crueller');
+const coconutBtn = document.getElementById ('coconut');
+const glazedBtn = document.getElementById ('glazed');
+const chocolateBtn = document.getElementById ('chocolate');
+const powderedBtn = document.getElementById ('powdered');
+
