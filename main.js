2fdd52984c2ad5a56e844e25a1a9ca0422e1b5cd
leelaur <laurenlee552@yahoo.com>
2022-03-01 18:13:08 -0500

    event listeners

--- main.js
+++ main.js
@@ -11,3 +11,80 @@ const glazedBtn = document.getElementById ('glazed');
 const chocolateBtn = document.getElementById ('chocolate');
 const powderedBtn = document.getElementById ('powdered');
 
+jellyBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+bostonBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+oldfBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+vanillaBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+strawberryBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+blueberryBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+barvarianBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+coconutBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+glazedBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+chocolateBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+powderedBtn.addEventListener('click', function() {
+    userPicked('Sco');
+});
+
+
+    // Zodiac Algorithm
+    if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
+    donut = "jelly";
+    } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+    donut = "boston";
+    } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
+    donut = "oldf";
+    } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+    donut = "vanilla";
+    } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+    donut = "strawberry";
+    } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+    donut = "blueberry";
+    } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+    donut = "barvarian";
+    } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+    donut = "crueller";
+    } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+    donut = "coconut";
+    } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+    donut = "glazed";
+    } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+    donut = "chocolate";
+    } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+    donut = "powdered";
+    }
+
+    console.log(AstroSign);
+    userPicked(AstroSign);
+});
+
+
